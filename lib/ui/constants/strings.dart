0fd8426185b339d9cd125dd57c8ec2dfda7cb081
Nilson Sasaki <nilsonsasaki@gmail.com>
2022-11-22 16:24:06 -0300

    feat: add strings file

---
+++ lib/ui/constants/strings.dart
@@ -0,0 +1,26 @@
+class Strings {
+  static const ptPrivacyPolicy = 'Política de privacidade';
+  static const ptPrivacyPolicyContent1 =
+      'Esta política de privacidade detalha como coletamos e processamos seus dados pessoais ao utilizar o aplicativo “Curso Solar USP".';
+  static const ptPrivacyPolicyContent2 =
+      'Sua privacidade é muito importante para nós, por isso coletamos e armazenamos o mínimo de informações para a utilização do aplicativo “Curso Solar USP”.';
+  static const ptPrivacyPolicyContent3 =
+      'Esta política de privacidade foi atualizada pela última vez em 22/11/2022.';
+  static const ptWhatDataAreCollected = 'Que dados coletamos?';
+  static const ptPersonalData = 'Dados pessoais';
+  static const ptPersonalDataContent = 'Não coletamos dados pessoais ou quaisquer informações que possam ser utilizados para identificar um indivíduo.';
+  static const ptUserGenerated = 'Conteúdo gerado pelo usuário';
+  static const ptUserGeneratedContent = 'Os conteúdos gerados pelos usuários processados durante o uso do  aplicativo "Curso Solar USP", não armazenamos permanentemente nenhuma informação gerada pelo usuário, elas são descartadas ao final de cada projeto';
+  static const ptSecurity = 'Segurança';
+  static const ptSecurityContent = 'Os seus dados pessoais e conteúdos gerados não são transmitidos ou armazenados permanentemente';
+  static const ptLinksToOtherWebsites = 'Links para sites de terceiros';
+  static const ptLinksToOtherWebsitesContent = 'Nosso aplicativo fornece links para websites que não são operados por nós portanto recomendamos que a política de privacidade destes sites seja revisada e entendida.';
+  static const ptAdvertising = 'Propaganda (Ads)';
+  static const ptAdvertisingContent = 'Nós não exibimos nenhum tipo de propaganda.';
+  static const ptUseStatistics = 'Estatísticas de uso';
+  static const ptUseStatisticsContent = 'Não coletamos nenhum dado sobre o uso do aplicativo.';
+  static const ptContactUs = 'Contate-nos';
+  static const ptContactUsContent = 'Caso tenha dúvidas ou sugestões sobre qualquer assunto relacionado a esta Política de Privacidade, entre em contato conosco através do e-mail nilsonsasaki@gmail.com.';
+  static const ptUpdate = 'Atualização';
+  static const ptUpdateContent = 'Nos reservamos o direito de revisar e modificar a Política de Privacidade a qualquer momento. A versão mais atualizada da nossa política de privacidade pode ser encontrada em nosso website https://nilsonsasaki.github.io/#/curso-solar-usp . Recomendamos que você visite regularmente esta página para se manter atualizado.';
+}
